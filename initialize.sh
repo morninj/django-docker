b95574a67e569ca173c0830bad363d7ef12bd977
Joseph Mornin <joseph@mornin.org>
2015-09-25 08:46:32 -0700

    Wait until MySQL daemon is running; and get root password from .ini

--- initialize.sh
+++ initialize.sh
@@ -5,6 +5,12 @@
 # If we're not in production, create a temporary dev database
 if [ "$DJANGO_PRODUCTION" != "true" ]; then
     echo "DJANGO_PRODUCTION=false; creating local database..."
+    # Wait until the MySQL daemon is running
+    until mysql -uroot -pdevrootpass -e ";" ; do
+        echo "MySQL daemon not running; waiting one second..."
+        sleep 1
+    done
+    echo "MySQL daemon is running; creating database..."
     mysql -uroot -e "CREATE DATABASE devdb; CREATE USER devuser@localhost; SET PASSWORD FOR devuser@localhost=PASSWORD('devpass'); GRANT ALL PRIVILEGES ON devdb.* TO devuser@localhost IDENTIFIED BY 'devpass'; FLUSH PRIVILEGES;" -pdevrootpass;
 else
     echo "DJANGO_PRODUCTION=true; no local database created"        
@@ -17,5 +23,4 @@ python /code/django_docker/manage.py migrate --noinput
 
 # Create a Django superuser named `root` if it doesn't yet exist
 echo "Creating Django superuser named 'root'..."
-export ROOT_PASSWORD=$(cat /code/root_password)
-echo "from django.contrib.auth.models import User; print 'Root user already exists' if User.objects.filter(username='root') else User.objects.create_superuser('root', 'admin@example.com', '$ROOT_PASSWORD')" | python /code/django_docker/manage.py shell
+echo "from ConfigParser import SafeConfigParser; parser = SafeConfigParser(); parser.read('/code/config.ini'); from django.contrib.auth.models import User; print 'Root user already exists' if User.objects.filter(username='root') else User.objects.create_superuser('root', 'admin@example.com', parser.get('general', 'ROOT_PASSWORD'))" | python /code/django_docker/manage.py shell
