4a8aaf3c9e1da5fd10580cc5a3859d801f2c9553
Joe Mornin <joe@mornin.org>
2016-10-16 12:13:56 -0700

    Update URLs to be compatible with Django 1.10

--- django_docker/django_docker/urls.py
+++ django_docker/django_docker/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+from hello_world import views as hello_world_views
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'hello_world.views.hello_world', name='hello_world'),
-)
+    url(r'^$', hello_world_views.hello_world, name='hello_world'),
+]
